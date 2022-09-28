e2e50927931c6094495a4a3c2723994cde328b2d
Kadir Yaren <uraniumstar20@gmail.com>
2022-09-29 01:09:31 +0300

    BUILD: HomePage desing coded

---
+++ src/pages/HomePage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Categories from '../components/Categories'
+import Header from '../components/Header'
+import Navigation from '../components/Navigation'
+import SearchBar from '../components/SearchBar'
+import Weather from '../components/Weather'
+
+export default function 
+() {
+  return (
+    <div className='homepage-container'>
+        <Header />
+        <Weather />
+        <SearchBar />
+        <Categories />
+        <Navigation />
+
+
+
+
+    </div>
+  )
+}
