7afeb3141dee51378a3ffe7d06ae94698a3151e7
Kadir Yaren <uraniumstar20@gmail.com>
2022-09-29 01:09:54 +0300

    BUILD: Categories desing coded

---
+++ src/pages/Categories.js
@@ -0,0 +1,24 @@
+import React, { useEffect,useState } from 'react'
+import Header from '../components/Categories/Header'
+import { useParams } from 'react-router-dom';
+import Subcategories from '../components/Categories/Subcategories';
+import Activities from '../components/Categories/Activities';
+import Navigation from '../components/Navigation';
+
+export default function Categories() {
+    const { category } = useParams();
+    const [categoryData, setCategoryData] = useState(category);
+
+    useEffect(() => {
+        console.log(categoryData);
+    },[categoryData])
+  return (
+    <div className='categories-container'>
+        <Header />
+        <Subcategories />
+        <Activities />
+        <Navigation />
+
+    </div>
+  )
+}
